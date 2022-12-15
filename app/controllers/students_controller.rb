d9899f18c0bc1a745a34255bd7be16a2d1da0f59
Cameron <Cameron@dansclan.com>
2022-12-15 15:19:34 -0800

    commit tree 128afc2101c457c89619c6684060d5d2cd15ef51

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
+    def index
+        if params[:name]
+            students =
+                Student.where("UPPER(first_name)=UPPER(?)", params[:name]).or(
+                    Student.where("UPPER(last_name)=UPPER(?)", params[:name]),
+                )
+            render json: students
+        else
+            students = Student.all
+            render json: students
+        end
+    end
 
-  def index
-    students = Student.all
-    render json: students
-  end
-
+    def show
+        student = Student.find(params[:id])
+        render json: student
+    end
 end
